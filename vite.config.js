714ffc8362480c40824dd27b1f11149a148d2a1e
Raumus204 <Enacra204@gmail.com>
2025-03-30 09:17:20 -0500

    host

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
+// filepath: c:\Users\enacr\coding\React-Project\vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
+  base: '/',
   server: {
     port: 3000,
     open: true,
+    host: '0.0.0.0', // Bind to all network interfaces
   },
   build: {
-    outDir: 'dist', 
+    outDir: 'dist',
   },
 })
